8f127bb8c22966c134f5ec478762f685c0494f82
capt-nemo429 <nemo429@protonmail.com>
2023-02-28 09:39:56 -0300

    fix: only display prices for spectrum verified assets

--- src/services/spectrumService.ts
+++ src/services/spectrumService.ts
@@ -33,7 +33,7 @@ class SpectrumService {
 
     const [ergPrice, data] = await Promise.all([
       coinGeckoService.getErgPrice(),
-      get<SpectrumPool[]>(new URL("v1/amm/markets", BASE_URL), {
+      get<SpectrumPool[]>(new URL("v1/amm/pools/summary", BASE_URL), {
         from: this._getUtcTimestamp(fromDate),
         to: this._getUtcTimestamp(new Date())
       })
