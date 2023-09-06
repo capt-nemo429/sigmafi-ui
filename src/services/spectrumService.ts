5c0f72bdde94e7ba221c9e3f3a522de5e74a5dbd
capt-nemo429 <nemo429@protonmail.com>
2023-09-06 17:10:15 -0300

    determine high liquidity spectrum pairs dynamically

--- src/services/spectrumService.ts
+++ src/services/spectrumService.ts
@@ -19,50 +19,66 @@ export type SpectrumPoolVolume = {
   value: number;
 };
 
+export type SpectrumPoolStat = {
+  id: string;
+  lockedX: SpectrumLockedValue;
+  lockedY: SpectrumLockedValue;
+  tvl: { value: number };
+};
+
+export type SpectrumLockedValue = {
+  id: string;
+  amount: number;
+  ticker: string;
+  decimals: number;
+};
+
 export type AssetPriceRate = {
   [tokenId: string]: { erg: number; fiat: number };
 };
 
-const HI_LIQ_ASSETS = [
-  "18c938e1924fc3eadc266e75ec02d81fe73b56e4e9f4e268dffffcb30387c42d",
-  "0cd8c9f416e5b1ca9f986a7f10a84191dfb85941619e49e53c0dc30ebf83324b",
-  "00b1e236b60b95c2c6f8007a9d89bc460fc9e78f98b09faec9449007b40bccf3",
-  "00bd762484086cf560d3127eb53f0769d76244d9737636b2699d55c56cd470bf",
-  "007fd64d1ee54d78dd269c8930a38286caa28d3f29d27cadcb796418ab15c283",
-  "e8b20745ee9d18817305f32eb21015831a48f02d40980de6e849f886dca7f807",
-  "1fd6e032e8476c4aa54c18c1a308dce83940e8f4a28f576440513ed7326ad489",
-  "089990451bb430f05a85f4ef3bcb6ebf852b3d6ee68d86d78658b9ccef20074f",
-  "003bd19d0187117f130b62e1bcab0939929ff5c7709f843c5c4dd158949285d0",
-  "03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04",
-  "02f31739e2e4937bb9afb552943753d1e3e9cdd1a5e5661949cb0cef93f907ea",
-  "d71693c49a84fbbecd4908c94813b46514b18b67a99952dc1e6e4791556de413",
-  "9a06d9e545a41fd51eeffc5e20d818073bf820c635e2a9d922269913e0de369d",
-  "472c3d4ecaa08fb7392ff041ee2e6af75f4a558810a74b28600549d5392810e8"
-];
-
 // https://api.spectrum.fi/v1/docs
 const BASE_URL = "https://api.spectrum.fi";
 const SPECTRUM_ERG_TOKEN_ID = "0000000000000000000000000000000000000000000000000000000000000000";
+const MIN_USD_LIQUIDITY = 10_000;
 
 class SpectrumService {
+  #highLiquidityTokenIds?: string[];
+
+  public async getPoolsStats(): Promise<SpectrumPoolStat[]> {
+    return get<SpectrumPoolStat[]>(new URL("v1/amm/pools/stats", BASE_URL));
+  }
+
+  public async getHighLiquidityTokenIds(): Promise<string[]> {
+    if (!this.#highLiquidityTokenIds) {
+      const stats = await this.getPoolsStats();
+      this.#highLiquidityTokenIds = stats
+        .filter((x) => x.lockedX.id === SPECTRUM_ERG_TOKEN_ID && x.tvl.value >= MIN_USD_LIQUIDITY)
+        .map((x) => x.lockedY.id);
+    }
+
+    return this.#highLiquidityTokenIds;
+  }
+
   public async getTokenRates(): Promise<AssetPriceRate> {
     const fromDate = new Date();
     fromDate.setDate(fromDate.getDate() - 30);
 
-    const [ergPrice, data] = await Promise.all([
+    const [ergPrice, markets, hiLiqTokens] = await Promise.all([
       coinGeckoService.getErgPrice(),
       get<SpectrumPool[]>(new URL("v1/price-tracking/markets", BASE_URL), {
         from: this._getUtcTimestamp(fromDate),
         to: this._getUtcTimestamp(new Date())
-      })
+      }),
+      this.getHighLiquidityTokenIds()
     ]);
 
-    const filtered = uniqWith(
-      data.filter((x) => x.baseId === SPECTRUM_ERG_TOKEN_ID && HI_LIQ_ASSETS.includes(x.quoteId)),
-      (a, b) => a.quoteId === b.quoteId && BigNumber(a.baseVolume.value).lt(b.baseVolume.value)
+    const pools = uniqWith(
+      markets.filter((p) => p.baseId === SPECTRUM_ERG_TOKEN_ID && hiLiqTokens.includes(p.quoteId)),
+      (a, b) => a.quoteId === b.quoteId && a.baseVolume.value <= b.baseVolume.value
     );
 
-    const dict = toDict(filtered, (r) => {
+    const dict = toDict(pools, (r) => {
       const erg = BigNumber(1).div(r.lastPrice);
 
       return {
