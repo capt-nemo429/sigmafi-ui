17f146efe65b003505def511e435ea44a6670c02
capt-nemo429 <nemo429@protonmail.com>
2024-08-15 14:52:06 -0300

    fetch verified assets rates even if has low liquidity

--- src/services/assetPricingService.ts
+++ src/services/assetPricingService.ts
@@ -5,6 +5,7 @@ import { uniqWith } from "lodash-es";
 import { graphQLService } from "./graphqlService";
 import { SPECTRUM_ERG_TOKEN_ID, spectrumService } from "./spectrumService";
 import { ERG_TOKEN_ID } from "@/constants";
+import { VERIFIED_ASSETS } from "@/maps/verifiedAssets";
 import { get } from "@/utils";
 
 const _1 = BigNumber(1);
@@ -15,6 +16,8 @@ const HODLERG3_TOKEN_ID = "cbd75cfe1a4f37f9a22eaee516300e36ea82017073036f07a09c1
 const HODLERG3_NFT_ID = "6e9c85c4be018b1308ddf034baf1406490e2a9dd406c01591bd6df41e98b6057";
 const HODLERG_CONTRACT =
   "100a040204000402040004000502050005d00f04040e2002a195c991b685f1bbf6c84cb192f698ecccc3c707b7293c21d27655ade8d56ed812d601db6308a7d602b27201730000d6038c720202d604b2a5730100d605db63087204d606b27205730200d6078c720602d6089972037207d609c17204d60a7ec1a706d60be4c6a70505d60c7e720b06d60de4c6a70405d60e9d9c720a720c7e99720d720306d60fe4c6a70605d610e4c6a70705d611e4c6a70805d61296830401927209720f93c27204c2a79683030193b27205730300b27201730400938c7206018c72020192720773059683050193e4c672040405720d93e4c672040505720b93e4c672040605720f93e4c672040705721093e4c6720408057211959172087306d1968302017212927e7209069a720a9d9c7e720806720e720cd803d6139d9c7e997207720306720e720cd6147307d615b2a5730800d1968303017212937e7209069a99720a72139d9c72137e7211067e72140696830201937ec17215069d9c72137e7210067e72140693cbc272157309";
+
+const VERIFIED_TOKEN_IDS = new Set(VERIFIED_ASSETS.map((x) => x.tokenId));
 
 type AssetRate = { erg: number; fiat: number };
 export type AssetPriceRates = {
@@ -39,7 +42,11 @@ async function getTokenRatesFromSpectrum(): Promise<Map<string, BigNumber>> {
 
   const map = new Map<string, BigNumber>();
   uniqWith(
-    markets.filter((p) => p.baseId === SPECTRUM_ERG_TOKEN_ID && hiLiqTokens.includes(p.quoteId)),
+    markets.filter(
+      (p) =>
+        VERIFIED_TOKEN_IDS.has(p.quoteId) ||
+        (p.baseId === SPECTRUM_ERG_TOKEN_ID && hiLiqTokens.has(p.quoteId))
+    ),
     (a, b) => a.quoteId === b.quoteId && a.baseVolume.value <= b.baseVolume.value
   ).map((pool) => map.set(pool.quoteId, _1.div(pool.lastPrice)));
 
@@ -73,15 +80,17 @@ async function getHodlErg3TokenRate(): Promise<BigNumber | undefined> {
 }
 
 class AssetPricingService {
-  #highLiquidityTokenIds?: string[];
+  #highLiquidityTokenIds?: Set<string>;
 
-  public async getHighLiquidityTokenIds(): Promise<string[]> {
+  public async getHighLiquidityTokenIds(): Promise<Set<string>> {
     if (!this.#highLiquidityTokenIds) {
       const stats = await spectrumService.getPoolsStats();
 
-      this.#highLiquidityTokenIds = stats
-        .filter((x) => x.lockedX.id === SPECTRUM_ERG_TOKEN_ID && x.tvl.value >= MIN_USD_LIQUIDITY)
-        .map((x) => x.lockedY.id);
+      this.#highLiquidityTokenIds = new Set(
+        stats
+          .filter((x) => x.lockedX.id === SPECTRUM_ERG_TOKEN_ID && x.tvl.value >= MIN_USD_LIQUIDITY)
+          .map((x) => x.lockedY.id)
+      );
     }
 
     return this.#highLiquidityTokenIds;
